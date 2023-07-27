b500271b2cb715276ac096bc950b3be5d5a2c2a9
Ryan Skinner <ryanskinner@gmail.com>
2023-07-26 20:27:05 -0400

    splitting up component

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -14,24 +14,6 @@ defineProps({
     <PostEmbedRecord :record="post.post.embed?.record" />
     <PostEmbedRecordWithMedia :images="post.post.embed?.media?.images" />
     <PostRecordEmbedDetail :record="post.post.embed?.record?.record" />
-
-    <div v-if="post.reason" class="mt-4">
-      <div class="text-gray-400 mb-2 font-mono text-sm">
-        Reposted by
-      </div>
-      <div class="flex items-center">
-        <div>
-          <img :src="post.reason.by.avatar" class="w-10 h-10 rounded-full">
-        </div>
-        <div class="flex flex-col ml-2">
-          <div class="font-mono text-sm">
-            {{ post.reason.by.displayName }}
-          </div>
-          <div class="text-gray-400 text-xs font-mono">
-            {{ post.reason.by.handle }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostReason :reason="post.reason" />
   </li>
 </template>
---
+++ src/components/post/PostReason.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+  reason: Object,
+})
+</script>
+
+<template>
+  <div v-if="reason" class="mt-4">
+    <div class="text-gray-400 mb-2 font-mono text-sm">
+      Reposted by
+    </div>
+    <div class="flex items-center">
+      <div>
+        <img :src="reason.by.avatar" class="w-10 h-10 rounded-full">
+      </div>
+      <div class="flex flex-col ml-2">
+        <div class="font-mono text-sm">
+          {{ reason.by.displayName }}
+        </div>
+        <div class="text-gray-400 text-xs font-mono">
+          {{ reason.by.handle }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
